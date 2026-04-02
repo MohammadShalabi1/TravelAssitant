cd246c5a6f24aba74295b8f8d1491ce1db6ed34f
MohammadShalabi1 <hassanaflaton11@gmail.com>
2026-04-02 18:00:17 +0300

    added a comment

--- backend/agent/memory.py
+++ backend/agent/memory.py
@@ -176,7 +176,7 @@ def save_message(session_id: str, role: str, content: str):
             )
         conn.commit()
 
-
+#load the history for a specific session (when clicking on a session in the history bar we need the fetch the messages in that session this what this fct do)
 def load_history(
     session_id: str,
     limit: int = 50,
